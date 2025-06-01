5dcbe889e9f4f9656b7f8a526d3dba73bded61af
Gabriel Fontes <hi@m7.rs>
2025-06-01 13:39:29 +0200

    chore: clean up flake a bit
    - Use explicit destructuring for inputs (avoids masking errors)
    - "Import" nixpkgs lib to be more organized
    - Use nix-systems instead of hardcoding secrets
    - Simplify forAllSystems
    - Remove `rec` from outputs
    - Simplify apps.deploy to use getExe

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    systems.url = "github:nix-systems/default";
     hardware.url = "github:nixos/nixos-hardware";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
@@ -41,23 +42,25 @@
   outputs = {
     self,
     nixpkgs,
+    systems,
+    deploy-rs,
     ...
   } @ inputs: let
     inherit (self) outputs;
-    systems = ["x86_64-linux" "aarch64-linux"];
-    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f { inherit system; pkgs = nixpkgs.legacyPackages.${system}; });
-  in rec {
+    inherit (nixpkgs.lib) genAttrs getExe;
+    forAllSystems = f: genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+  in {
     nixosConfigurations = {
       adelie = nixpkgs.lib.nixosSystem {
         modules = [./hosts/servers/adelie/configuration.nix];
         specialArgs = {inherit inputs outputs;};
       };
-    /*
+      /*
       galapagos = nixpkgs.lib.nixosSystem {
         modules = [./hosts/servers/galapagos/configuration.nix];
         specialArgs = {inherit inputs outputs;};
       };
-    */
+      */
       emperor = nixpkgs.lib.nixosSystem {
         modules = [./hosts/servers/emperor/configuration.nix];
         specialArgs = {inherit inputs outputs;};
@@ -73,14 +76,14 @@
     };
 
     deploy.nodes = let
-      activate = kind: config: inputs.deploy-rs.lib.${config.pkgs.system}.activate.${kind} config;
+      activate = kind: config: deploy-rs.lib.${config.pkgs.system}.activate.${kind} config;
     in {
       adelie = {
         hostname = "adelie.gelos.club";
         sshUser = "admin";
         profiles.system = {
           user = "root";
-          path = activate "nixos" nixosConfigurations.adelie;
+          path = activate "nixos" self.nixosConfigurations.adelie;
         };
         remoteBuild = true;
       };
@@ -90,40 +93,40 @@
         sshOpts = ["-p" "2112"];
         profiles.system = {
           user = "root";
-          path = activate "nixos" nixosConfigurations.emperor;
+          path = activate "nixos" self.nixosConfigurations.emperor;
         };
         remoteBuild = true;
       };
-      /* Temporariamente desativados
+      /*
       galapagos = {
         hostname = "galapagos.gelos.club";
         sshUser = "admin";
         sshOpts = ["-p" "2112"];
         profiles.system = {
           user = "root";
-          path = activate "nixos" nixosConfigurations.galapagos;
+          path = activate "nixos" self.nixosConfigurations.galapagos;
         };
         remoteBuild = true;
       };
       */
     };
 
-    packages = forAllSystems ({pkgs, system, ...}: rec {
-      site = pkgs.callPackage ./site/default.nix { inherit atas; };
+    packages = forAllSystems (pkgs: rec {
+      site = pkgs.callPackage ./site/default.nix {inherit atas;};
       atas = pkgs.callPackage ./site/atas.nix {
-        beamer-theme = inputs.gelos-beamer-theme.packages.${system}.theme;
+        beamer-theme = inputs.gelos-beamer-theme.packages.${pkgs.system}.theme;
       };
-      identidade-visual = pkgs.callPackage ./identidade-visual/default.nix { };
+      identidade-visual = pkgs.callPackage ./identidade-visual/default.nix {};
     });
 
-    apps = forAllSystems ({system, ...}: rec {
+    apps = forAllSystems (pkgs: rec {
       deploy = {
         type = "app";
-        program = "${nixpkgs.legacyPackages.${system}.deploy-rs}/bin/deploy";
+        program = getExe pkgs.deploy-rs;
       };
       default = deploy;
     });
 
-    formatter = forAllSystems ({system, ...}: nixpkgs.legacyPackages.${system}.alejandra);
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
   };
 }
