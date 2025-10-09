958c6853f8afa694c1849068d0585892d08fa62b
Gabriel Fontes <hi@m7.rs>
2025-10-09 17:33:04 -0300

    chore: turn eval-jobs.py into derivation

--- flake.nix
+++ flake.nix
@@ -124,6 +124,40 @@
         type = "app";
         program = getExe pkgs.deploy-rs;
       };
+      github-eval-checks = {
+        type = "app";
+        # Use nix-eval-jobs to evaluate all checks, and convert into a format github-actions can use as matrix
+        # The difference between this and nix-github-actions is that our matrix will have access to the out/drvPath as well as cached status
+        program = getExe (pkgs.writers.writePython3Bin "github-eval-checks" { flakeIgnore = ["E501"]; } ''
+          import json
+          import subprocess
+
+          # From https://github.com/nix-community/nix-github-actions
+          githubPlatforms = {
+              "x86_64-linux": "ubuntu-24.04",
+              "x86_64-darwin": "macos-13",
+              "aarch64-darwin": "macos-14",
+              "aarch64-linux": "ubuntu-24.04-arm",
+          }
+          command = subprocess.run(["${getExe pkgs.nix-eval-jobs}", "--force-recurse", "--check-cache-status", "--flake", ".#checks"], capture_output=True)
+          jobs = map(json.loads, command.stdout.decode('utf-8').splitlines())
+          matrix = {'include': []}
+          for job in jobs:
+              [system, name] = job['attrPath']
+              attr = f"checks.{system}.{name}"
+              matrix['include'].append({
+                  'name': name,
+                  'system': system,
+                  'attr': attr,
+                  'os': githubPlatforms[system],
+                  'isCached': job['isCached'],
+                  'drvPath': job['drvPath'],
+                  'outPath': job['outputs']['out']
+              })
+
+          print(json.dumps(matrix))
+        '');
+      };
       default = deploy;
     });
 
